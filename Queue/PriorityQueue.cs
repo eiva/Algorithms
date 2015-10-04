811ecbe19c8b04c08f4daf515fa566afff7630ec
Eugene Ivanchenko <astellar@gmail.com>
2015-10-04 12:20:01 -0400

    Change queue contract

--- Queue/PriorityQueue.cs
+++ Queue/PriorityQueue.cs
@@ -1,8 +1,18 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace Queue
 {
-	public interface IPriorityQueue<TK, TV> 
+	public interface IQueue<TV>
+	{
+		void Enqueue(TV value);
+		bool IsEmpty {get;}
+		TV Dequeue();
+	}
+
+	/// <summary>
+	/// Priority queue, Value with smallest key will be returned first.
+	/// </summary>
+	public interface IPriorityQueue<TK, TV> : IQueue<KeyValuePair<TK, TV>>
 		where TK:IComparable<TK>
 	{
 		void Enqueue(TK key, TV value);
