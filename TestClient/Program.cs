d6d8bde891f830be9a9ae40cb84a200b04227b57
Eugene Ivanchenko <astellar@gmail.com>
2015-10-11 19:10:00 -0400

    MultiDictionary initial implementation

--- TestClient/Program.cs
+++ TestClient/Program.cs
@@ -1,11 +1,65 @@
 using System;
+using System.Collections.Generic;
+using Utils;
+using System.Linq;
 
 namespace TestClient
 {
+	
+	public sealed class MultiDictionary<TK, TV>
+		where TV : IEquatable<TV>
+	{
+		IDictionary<TK, List<KeyValuePair<TK,TV>>> _map = new Dictionary<TK, List<KeyValuePair<TK,TV>>> ();
+		public void Add(TK key, TV value)
+		{
+			var chain = _map.GetOrCreate(key, ()=>{return new List<KeyValuePair<TK,TV>>();});
+			chain.Add (new KeyValuePair<TK, TV> (key, value));
+		}
+
+		public IEnumerable<TV> Get(TK key)
+		{
+			return _map [key].Select (p => p.Value);
+		}
+
+		/// <summary>
+		/// Removes all entrances of specifyed value at specifyed key.
+		/// </summary>
+		/// <param name="key">Key.</param>
+		/// <param name="value">Value.</param>
+		public void RemoveAll(TK key, TV value)
+		{
+			var chain = _map.GetOrCreate(key, ()=>{return new List<KeyValuePair<TK,TV>>();});
+			chain.RemoveAll ( p => {return Equals(value, p.Value);});
+			if (chain.Count == 0) {
+				_map.Remove (key);
+			}
+		}
+		/// <summary>
+		/// Removes all entrances at specifyed key.
+		/// </summary>
+		/// <param name="key">Key.</param>
+		/// <param name="value">Value.</param>
+		public void RemoveAll(TK key)
+		{
+			_map.Remove (key);
+		}
+
+		public void Clear()
+		{
+			_map.Clear ();
+		}
+
+		IEnumerable
+	}
 	class MainClass
 	{
 		public static void Main (string[] args)
 		{
+			IReadOnlyDictionary<int, int> f;
+			var d = new MultiDictionary<int, string> ();
+			d.Add (1, "1");
+			d.Add (1, "2");
+			Console.WriteLine (String.Join (", ", d.Get (1)));
 		}
 	}
 }
--- Utils/DictionaryHelper.cs
+++ Utils/DictionaryHelper.cs
@@ -25,6 +25,27 @@ namespace Utils
 			dict.Add(key, res);
 			return res;
 		}
+
+		/// <summary>
+		/// Gets existing value or create new using new() from specified dictionary.
+		/// </summary>
+		/// <returns>The or create.</returns>
+		/// <param name="dict">Dict.</param>
+		/// <param name="key">Key.</param>
+		/// <typeparam name="TK">The 1st type parameter.</typeparam>
+		/// <typeparam name="TV">The 2nd type parameter.</typeparam>
+		public static TV GetOrCreate<TK, TV>(this IDictionary<TK, TV> dict, TK key, Func<TV> factory)
+			where TV : class
+		{
+			TV res;
+			if (dict.TryGetValue(key, out res))
+			{
+				return res;
+			}
+			res = factory();
+			dict.Add(key, res);
+			return res;
+		}
 	}
 }
 
